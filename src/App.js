91e5fcfaee6ccb61efe24628c80956f4c2f0e1a9
Jaime <jamoncayop@unal.edu.co>
2022-03-21 16:22:20 -0500

    tercer commit Jaime

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import esposos from "../src/assets/images/esposos.svg";
 import Body from "./components/body/Body";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       Hola Mundo
       <img src={esposos} alt=""></img>
-      <Body/>
+      <Body />
+      <Footer />
     </div>
   );
 }
