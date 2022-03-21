cfea76a1118ad7e41a58e4158fcda0ea45b0d501
Jaime <jamoncayop@unal.edu.co>
2022-03-21 14:37:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import esposos from "../src/assets/images/esposos.svg";
+
+function App() {
+  return (
+    <div className="App">
+      Hola Mundo
+      <img src={esposos} alt=""></img>
+    </div>
+  );
+}
+
+export default App;
