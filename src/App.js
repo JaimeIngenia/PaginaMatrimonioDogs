11fd7713eb77291e4621f589b1022ba20e007d88
DilbaniE <claudiaenriquez241@gmail.com>
2022-03-21 16:13:22 -0500

    primer comit dilbaniii

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import esposos from "../src/assets/images/esposos.svg";
+import Body from "./components/body/Body";
 
 function App() {
   return (
     <div className="App">
       Hola Mundo
       <img src={esposos} alt=""></img>
+      <Body/>
     </div>
   );
 }
