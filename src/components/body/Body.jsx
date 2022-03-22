39032096a2c5eb83cffc8b6737033479be4ee4e9
Jaime <jamoncayop@unal.edu.co>
2022-03-22 10:47:08 -0500

    Sexto Commit Jaime

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Body = () => {
-  return <div>hole soy body dilbani</div>;
+  return <div  >hole soy body dilbani
+    <Link to="/historias" >
+      <button>Pasar a otro componente</button>
+    </Link>
+  </div>;
 };
 
 export default Body;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// router
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppProvider } from "./components/context/AppContext";
+//components
+import Historias from "./components/hisroeias/Historias";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppProvider>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route exact path="/historias" element={<Historias />} />
+        </Routes>
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
